905f6d4a7bd37d3eff6eed711789337b4d77519e
Michał Orzechowski <m.orzechowski@adform.com>
2022-08-29 08:37:26 +0200

    next steps

--- index.js
+++ index.js
@@ -15,7 +15,13 @@ function animate(){
     car.update();
     canvas.height = window.innerHeight;
 
+    context.save();
+    context.translate(0, -car.y+canvas.height*0.7);
+
     road.draw(context);
     car.draw(context);
+
+    context.restore();
+
     requestAnimationFrame(animate);
 }
